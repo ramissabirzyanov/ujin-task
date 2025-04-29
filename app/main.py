e62ceb4932e01deeb313a8ea0636503785b30558
Ramis <ramis@Ramiss-Air.Dlink>
2025-04-29 16:45:58 +0300

    service does not know about parsing

--- app/main.py
+++ app/main.py
@@ -3,14 +3,14 @@ from fastapi import FastAPI
 
 from app.api.endpoints import router as api_router
 from app.core.setup_parser import setup_parser
-from app.api.dependencies import set_parsed_args, is_debug
+from app.api.dependencies import set_parsed_args
 
-
+args = setup_parser()
+set_parsed_args(args)
 app = FastAPI()
 app.include_router(api_router, prefix='/api')
 
 
 if __name__ == "__main__":
-    args = setup_parser()
-    set_parsed_args(args)
+
     uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
